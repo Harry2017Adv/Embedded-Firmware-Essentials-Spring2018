ce85d308bf389fbf863d11dc72523cf0f58ff469
wachenda <david.wachenschwanz@mac.com>
2018-06-01 19:18:31 -0700

    Updating Lect5_ADXL345_Modular

--- Lect5_ADXL345_Modular/main.cpp
+++ Lect5_ADXL345_Modular/main.cpp
@@ -1,6 +1,6 @@
 /*********************************************************************************************************/
 //
-// Lecture 4 - ADXL345 Using SPI
+// Lecture 5 - Modular Programming: ADXL345 Using SPI
 //
 //
 //
@@ -8,30 +8,23 @@
 #include "mbed.h"
 #include "ADXL345.h"
 
-
-//SPI acc(p11,p12,p13);  // MOSI (SDA), MISO (SDO), SCL
-//DigitalOut cs(p14);
 Serial pc(USBTX,USBRX);
 
 
-float x, y, z;
 
 int main() 
 {
+	float x, y, z;
+
 	pc.baud(115200);
 	pc.printf("Starting ADXL345 Test...\r\n\r\n");
 	// Initialize ADXL345
 	ADXL345_init();
 
-
 	while(1)
 	{
 		wait_ms(200);
-
-
 		ADXL345_meas();
-
-
 		x = acc_x();
 		y = acc_y();
 		z = acc_z();
